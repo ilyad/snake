772b0453483268b43927547a7779d43741ae4245
Ilya Dogolazky <ilya@eee.local>
2010-10-13 22:21:02 +0300

    a speccy z80 tool chain

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+template="randomize-usr-30000.sna" &&
+z80asm a.asm &&
+let sna_offset=30000-16384+27 &&
+a_bin_len=$(wc -c a.bin | awk '{print $1}') &&
+let tail=49179-$sna_offset-$a_bin_len &&
+( head -c $sna_offset $template &&
+  cat a.bin &&
+  tail -c $tail $template ) > a.sna &&
+wc -c a.sna &&
+wc -c a.bin
+
